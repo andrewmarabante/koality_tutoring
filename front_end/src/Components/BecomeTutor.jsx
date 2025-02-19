22887ae7e36d0010b9e242da735a48c846975910
andrewmarabante <andrewmarabante@gmail.com>
2025-02-19 11:36:42 -0800

    BecomeTutor is now responsive

--- front_end/src/Components/BecomeTutor.jsx
+++ front_end/src/Components/BecomeTutor.jsx
@@ -1,59 +1,59 @@
 export default function BecomeTutor(){
     return(
         <div className="flex-col grow justify-center items-center z-40 p-10 overflow-hidden relative">
-            <div className="h-full relative">
-                <div className="bg-[rgba(255,255,255,0.75)] rounded-2xl p-5 h-full overflow-scroll">
-                    <div className="text-4xl font-roboto border-b text-center mx-3 pb-3">Become a Tutor!</div>
-                    <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Competitive Pay:</div>
-                    <div className="text-sm font-roboto px-2">
-                        Creating your own tutoring business can be very lucrative and offer you an actual
-                        career path as an educator! If you have the charisma and passion to teach and are
-                        knowledgeable in your field, there are many students that need you!!
-                    </div>
-                    <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Flexible Schedule</div>
-                    <div className="text-sm font-roboto px-2">
-                        Having your own business means you choose your own schedule! Need the weekend off to
-                        shred some gnar with your bros? No problem!
-                    </div>
-                    <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Consistent Learning</div>
-                    <div className="text-sm font-roboto px-2">
-                        Becoming a tutor means you are consistently learning and strengthening your knowledge
-                        in your field. Everyone has gaps in knowledge and teaching something is the best way 
-                        to find and work through yours! 
-                    </div>
-                    <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Subjects we're looking for:</div>
-                    <div className="text-sm font-roboto px-2">
-                        Anything STEM related has more opportunity as these are more objective classes, students
-                        tend to need more help. However, we need tutors for literally everything so get in touch!
-                    </div>
-                    <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Subjects we <span className="font-roboto-title-italic">Need</span>:</div>
-                    <div className="text-sm font-roboto px-2">
-                        Koality Tutoring is a new company, as such we have a <em>bunch</em> of holes we need filled: 
-                        <div className="grid grid-cols-3 my-2">
-                            <div>Math</div>
-                            <div>Stats</div>
-                            <div>Chemistry</div>
-                            <div>Biology</div>
-                            <div>Econ</div>
-                            <div>English</div>
-                            <div>History</div>
-                            <div>Psychology</div>
-                            <div>CompSci</div>
+            <div className="h-full relative flex justify-center">
+                <div className="bg-[rgba(255,255,255,0.75)] rounded-2xl h-full md:w-3/4 md:px-20 relative xl:w-1/3">
+                <div className='h-5 w-full bg-gradient-to-t from-white to-transparent z-20 opacity-90 absolute bottom-0 left-0 rounded-4xl'></div>
+
+                    <div className="p-5 h-full overflow-scroll">
+                        <div className="text-4xl font-roboto border-b text-center mx-3 pb-3">Become a Tutor!</div>
+                        <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Competitive Pay:</div>
+                        <div className="text-sm font-roboto px-2">
+                            Creating your own tutoring business can be very lucrative and offer you an actual
+                            career path as an educator! If you have the charisma and passion to teach and are
+                            knowledgeable in your field, there are many students that need you!!
                         </div>
-                        Apply for these as soon as possible to get priority!
+                        <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Flexible Schedule</div>
+                        <div className="text-sm font-roboto px-2">
+                            Having your own business means you choose your own schedule! Need the weekend off to
+                            shred some gnar with your bros? No problem!
+                        </div>
+                        <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Consistent Learning</div>
+                        <div className="text-sm font-roboto px-2">
+                            Becoming a tutor means you are consistently learning and strengthening your knowledge
+                            in your field. Everyone has gaps in knowledge and teaching something is the best way
+                            to find and work through yours!
+                        </div>
+                        <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Subjects we're looking for:</div>
+                        <div className="text-sm font-roboto px-2">
+                            Anything STEM related has more opportunity as these are more objective classes, students
+                            tend to need more help. However, we need tutors for literally everything so get in touch!
+                        </div>
+                        <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">Subjects we <span className="font-roboto-title-italic">Need</span>:</div>
+                        <div className="text-sm font-roboto px-2">
+                            Koality Tutoring is a new company, as such we have a <em>bunch</em> of holes we need filled:
+                            <div className="grid grid-cols-3 my-2">
+                                <div>Math</div>
+                                <div>Stats</div>
+                                <div>Chemistry</div>
+                                <div>Biology</div>
+                                <div>Econ</div>
+                                <div>English</div>
+                                <div>History</div>
+                                <div>Psychology</div>
+                                <div>CompSci</div>
+                            </div>
+                            Apply for these as soon as possible to get priority!
+                        </div>
+                        <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">How to apply:</div>
+                        <div className="text-sm font-roboto px-2">
+                            Head over to the Schedules tab and select a time that works with your schedule. Then,
+                            just send a text message to <span className="font-roboto-title-italic">(951)348-1243</span> with your Name, Subject, and Schedule time.
+                            Be sure to specify that you want to be a tutor, not a student! We will be in touch to have an
+                            interview at that time!
+                        </div>
                     </div>
-                    <div className="text-xl font-roboto pt-5 px-2 pb-1 w-fit">How to apply:</div>
-                    <div className="text-sm font-roboto px-2">
-                        Head over to the Schedules tab and select a time that works with your schedule. Then,
-                        just send a text message to <span className="font-roboto-title-italic">(951)348-1243</span> with your Name, Subject, and Schedule time. 
-                        Be sure to specify that you want to be a tutor, not a student! We will be in touch to have an
-                        interview at that time!
-                    </div>
-
-
-
                 </div>
-                <div className='h-5 w-full bg-gradient-to-t from-white to-transparent z-20 opacity-90 absolute bottom-0 rounded-4xl'></div>
             </div>
         </div>
     )
